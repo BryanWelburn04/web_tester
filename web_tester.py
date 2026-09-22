fa05b8cafbcccd94282d18bc88d8238d2af3793d
BryanWelburn04 <bryanwelburn04@gmail.com>
2026-09-22 16:15:33 -0700

    Created function to splice URI and return data

--- web_tester.py
+++ web_tester.py
@@ -1,5 +1,28 @@
+import sys
+
 def parse_uri(uri):
-    return
+    # URI format: protocol://host[:port]/filepath
+    # NOTE ADD HANDLING FOR URI'S WITH NO PORT
+
+    uri_data = []
+
+    uri = uri.split("://")
+    uri_data.append(uri[0])
+    print(uri[0])
+
+    uri = uri[1].split("[:")
+    uri_data.append(uri[0])
+    print(uri[0])
+
+    uri = uri[1].split("]/")
+    uri_data.append(uri[0])
+    print(uri[0])
+
+    uri = uri[1].split("]/")
+    uri_data.append(uri[0])
+    print(uri[0])
+
+    return uri_data
 
 def open_connection(host, port, use_tls):
     return
@@ -26,4 +49,16 @@ def check_password_protection(status_code):
     return
 
 def main():
+    print("======================")
+    if len(sys.argv) < 2:
+        print("Error: Too few arguements provided, please provide a URI")
+        return
+    if len(sys.argv) > 2:
+        print("Error: Too many arguements provided, please provide only a URI")
+        return
+    unparsed_uri = sys.argv[1]
+    parsed_uri = parse_uri(unparsed_uri)
+    print(parsed_uri)
     return
+
+main()
