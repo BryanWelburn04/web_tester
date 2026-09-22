b71c45a2676f0896117662996b3e9e66654c9162
BryanWelburn04 <bryanwelburn04@gmail.com>
2026-09-22 11:55:42 -0700

    Created function placeholders

---
+++ web_tester.py
@@ -0,0 +1,29 @@
+def parse_uri(uri):
+    return
+
+def open_connection(host, port, use_tls):
+    return
+
+def send_http_request(socket, request):
+    return
+
+def receive_request(socket):
+    return
+
+def parse_response(response):
+    return
+
+def handle_redirects():
+    return
+
+def check_http2_support():
+    return
+
+def extract_cookies(headers):
+    return
+
+def check_password_protection(status_code):
+    return
+
+def main():
+    return
